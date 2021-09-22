e44a62deab498f0424986301af886e20372b649a
LeopoldArkham <leopold.arkham@gmail.com>
2021-09-23 00:31:43 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 extern crate rustygit;
 extern crate rm_rf;
+extern crate httparse;
+extern crate serde_derive;
+extern crate serde_json;
 
 use std::io::prelude::*;
+use std::collections::HashMap;
 use std::process::Command;
 use std::error::Error;
 use std::net::TcpListener;
 
 mod read_config;
 
+use serde_derive::Deserialize;
+
+use httparse::Request;
+
 use rustygit::types::BranchName;
 use std::str::FromStr;
 
 use read_config::{read_config, Recipe};
 use rustygit::types::GitUrl;
+
+#[derive(Hash, PartialEq, Eq)]
+struct Target {
+    repoository_name: String,
+    branch_name: String
+}
+
+type Repos = HashMap<Target, rustygit::Repository>;
 
 // Goes through the list of recipes and clones all the repos
 fn initialize(recipes: &Vec<Recipe>) -> Result<(), Box<dyn Error>> {
@@ -41,24 +57,61 @@ fn initialize(recipes: &Vec<Recipe>) -> Result<(), Box<dyn Error>> {
     return Ok(());
 }
 
+#[derive(Debug, Deserialize)]
+struct Repository {
+    name: String
+}
+
+#[derive(Debug, Deserialize)]
+struct Webhook {
+    r#ref: String,
+    repository: Repository,
+}
 
 fn main() -> std::io::Result<(), > {
-    let _ = rm_rf::ensure_removed("./repos");
-    // let recipes: Vec<Recipe> = read_config()?.recipes;
-    let listener = TcpListener::bind("127.0.0.1:6000").unwrap();
-
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        let mut buffer = [0; 20_000];
-        println!("Connection established!");
-        stream.read(&mut buffer).unwrap();
-
-        let res = String::from_utf8_lossy(&buffer);
-        
-        println!("{}", res);
-
-    }
-    
+    let repos = read_config();
 
     Ok(())
 }
+
+// fn main() -> std::io::Result<(), > {
+//     let _ = rm_rf::ensure_removed("./repos");
+//     // let recipes: Vec<Recipe> = read_config()?.recipes;
+//     let listener = TcpListener::bind("127.0.0.1:6000").unwrap();
+
+//     for stream in listener.incoming() {
+//         let mut stream = stream.unwrap();
+//         let mut buffer = [0; 20_000];
+//         let mut headers = [httparse::EMPTY_HEADER; 20];
+//         println!("\n\n");
+//         let bytes_read = stream.read(&mut buffer).unwrap();
+
+//         let offset = Request::new(&mut headers).parse(&buffer).unwrap().unwrap();
+
+//         // println!("Hopefully body: {}", String::from_utf8_lossy(&buffer[offset..]));
+//         let body = String::from_utf8_lossy(&buffer[offset..bytes_read]).to_string();
+//         println!("Body: {:?}", &body[body.len() - 1..]);
+//         println!("Body length: {:?}", body.len());
+//         let maybe_parsed_webhook: Webhook = serde_json::from_str(&body)?;
+//         println!("Parsed: {:?}", maybe_parsed_webhook);
+
+//         println!("------------------------------------------------");
+
+//     }
+    
+
+//     Ok(())
+// }
+
+
+/*
+ * main {
+ *   read_config
+ *   loop over tcp incoming {
+ *     if name and branch in config {
+ *       pull updated repo
+ *       build it++
+ *     } 
+ *   }
+ * }
+ */
